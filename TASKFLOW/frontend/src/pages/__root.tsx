2be3007272f3f6297208077c3a465bd0306b7121
Progyan <janghyunseung13@gmail.com>
2025-05-19 19:09:57 +0530

    Updated the packages

--- TASKFLOW/frontend/src/pages/__root.tsx
+++ TASKFLOW/frontend/src/pages/__root.tsx
@@ -1,12 +1,12 @@
-import { createRootRoute } from "@tanstack/react-router";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 export const Route = createRootRoute({
   component: () => (
     <>
-      <div className="flex gap-2 p-2">Hello</div>
+      <div className="flex gap-2 p-2"></div>
       <hr />
-
+      <Outlet />
       <TanStackRouterDevtools />
     </>
   ),
